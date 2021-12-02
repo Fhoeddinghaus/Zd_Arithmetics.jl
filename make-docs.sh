15eaee2f17e1c0b9eb10265d5937f5fa3bdd98fe
Fhoeddinghaus <info@xefli.de>
2021-12-03 00:31:15 +0100

    Rebuild GitHub pages

---
+++ make-docs.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+cd docs
+git commit -am "Save uncommited changes (WIP)"
+julia make.jl
+git branch --delete --force gh-pages
+git checkout --orphan gh-pages
+git reset
+
+rm -rf ../src
+# rm ../Manifest.toml
+rm ../Project.toml
+rm ../README.md
+cp -r build/ ../
+
+cd ..
+rm -rf docs
+
+git add --all
+git commit -m "Rebuild GitHub pages"
+git push -f origin gh-pages && git checkout main
